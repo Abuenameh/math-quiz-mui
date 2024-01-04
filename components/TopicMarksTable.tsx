47dfa54d67cc9383d34005595c481f11fb4f8123
Abuenameh <abuenameh@gmail.com>
2024-01-04 04:57:20 -0400

    Commit

--- components/TopicMarksTable.tsx
+++ components/TopicMarksTable.tsx
@@ -5,7 +5,7 @@ import Box from "@mui/material/Box";
 export const TopicMarksTable = ({marks}: { marks: MarkStats[] }) => {
     const columns: GridColDef[] = [
         { field: "name", headerName: "Topic name", width: 300 },
-        { field: "description", headerName: "Topic description", flex: 1 },
+        { field: "description", headerName: "Topic description", flex: 1, renderCell: (params) => (<div style={{whiteSpace: "pre-line"}}>{params.value}</div>) },
         { field: "mark", headerName: "Mark", width: 100 },
         { field: "total", headerName: "Out of", width: 100 },
     ];
@@ -21,7 +21,7 @@ export const TopicMarksTable = ({marks}: { marks: MarkStats[] }) => {
     return (
         <>
             <Box className={"max-h-[20rem]"}>
-            <DataGrid sx={{"& .MuiDataGrid-row:hover": {cursor: "default", background: "transparent"}, "& .MuiDataGrid-row.Mui-hovered": {background: "transparent"}}} columns={columns} rows={rows} disableRowSelectionOnClick disableColumnFilter disableColumnSelector disableDensitySelector
+            <DataGrid getRowHeight={() => 'auto'} sx={{"& .MuiDataGrid-row:hover": {cursor: "default", background: "transparent"}, "& .MuiDataGrid-row.Mui-hovered": {background: "transparent"}}} columns={columns} rows={rows} disableRowSelectionOnClick disableColumnFilter disableColumnSelector disableDensitySelector
             initialState={{
                 sorting: { sortModel: [{ field: "name", sort: "asc" }] },
             //     pagination: { paginationModel: { pageSize: 10 } },
